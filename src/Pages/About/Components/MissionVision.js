d31bcd17307d1a3f7ec8698543e66303f5a4d14a
Kevin <kevin@Kevins-MacBook-Pro-2.local>
2019-03-07 19:51:31 -0800

    update about copy

--- src/Pages/About/Components/MissionVision.js
+++ src/Pages/About/Components/MissionVision.js
@@ -17,7 +17,7 @@ const Container = styled.div`
 
 const Content = {
     p1: "Founded in 2018, Aegis Custody is a San Francisco fintech company that provides financial institutions with a secure and easy-to-use solution for custody of digital assets. We are backed by Fosun, a global asset management company.",
-    p2: "Our mantra is ‘security first’ because we take security very seriously. This ideology carries through everything we do from hardware design, to software deployment, and internal operations. Digital assets need to be effectively secured and easy-to-use. We distribute trust across an organization and implement transfer policies that require multiple approvals to move assets. Each step in the process is easy-to-do, but when combined together creates a powerful defense.",
+    p2: "Digital assets need to be effectively secured and easy-to-use. Our mantra is ‘security first’ because we take security very seriously. This ideology carries through everything we do from hardware design, to software deployment, and internal operations.",
 
 }
 
